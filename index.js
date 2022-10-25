c888a98f8fb1021d36b8c20b8f6f15211fd0d760
Christopher Shelley <dont@forgetmy.email>
2022-10-25 11:59:27 +0200

    set up cors and test html file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import usersRouter from "./users/routes.js"
 import ordersRouter from "./orders/routes.js"
 import { connectToDB } from "./database.js"
+import cors from "cors"
 
 const PORT = process.env.PORT || 8080
 const api = express()
@@ -9,6 +10,14 @@ const api = express()
 connectToDB()
     .then(()=>{
         api.use(express.json())
+        // i want you to allow CROSS-ORIGIN REQUESTS
+
+        // 2 completely different origins
+        // your express API > heroku.com 
+        // your react client > netlify.com 
+
+
+        api.use(cors())
 
         api.use("/users", usersRouter)
         api.use("/orders", ordersRouter)
